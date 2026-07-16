ea19695095ab3869e982e3cda16832d6d8a7f9dc
Zuriplayer <zuriplayer@gmail.com>
2026-07-16 12:08:48 +0200

    Create main window scaffold for EZORaidPlanner
    Added a scaffold for the in-game window with a toggle function.

---
+++ modules/main_window.lua
@@ -0,0 +1,7 @@
+EZORaidPlanner = EZORaidPlanner or {}
+EZORaidPlanner.MainWindow = EZORaidPlanner.MainWindow or {}
+
+-- In-game window scaffold. UI implementation pending.
+function EZORaidPlanner.MainWindow.Toggle()
+    -- TODO: build the event list / creation / results UI.
+end
